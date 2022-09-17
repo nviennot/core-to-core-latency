58ed12110ba40822c793d5575787203b3d6600dc
Nicolas Viennot <nicolas@viennot.biz>
2022-09-17 18:22:00 -0400

    Add stats

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
+use ansi_term::Color;
 use core_affinity::CoreId;
+use ndarray::{Axis, s};
+use ordered_float::NotNan;
 use std::sync::atomic::{AtomicU8, Ordering};
 use std::io::Write;
+use clap::Parser;
 
 #[cfg(feature = "rdtsc")]
 use minstant::Instant;
 #[cfg(not(feature = "rdtsc"))]
 use std::time::Instant;
-use clap::Parser;
 
-const DEFAULT_NUM_ROUND_TRIP: RoundTrips = 30_000;
-type RoundTrips = u32;
+const DEFAULT_NUM_SAMPLES: Count = 300;
+const DEFAULT_NUM_ROUND_TRIPS: Count = 1000;
+type Count = u32;
 
 // This is a little matchup between two threads
 // One sends a ball (Ping!), and the other sends it back (Pong!)
 pub fn bench(
     (ping_core, pong_core): (CoreId, CoreId),
-    num_round_trips: RoundTrips,
-) -> std::time::Duration {
+    num_round_trips: Count,
+    num_samples: Count,
+) -> Vec<std::time::Duration> {
     #[derive(PartialEq, Eq)]
     #[repr(u8)]
     enum State {
@@ -36,7 +41,7 @@ pub fn bench(
             // Announce our presence
             state.store(Pong as u8, Ordering::Relaxed);
 
-            for _ in 0..num_round_trips {
+            for _ in 0..(num_round_trips*num_samples) {
                 while state.compare_exchange(Ping as u8, Pong as u8, Ordering::Relaxed, Ordering::Relaxed).is_err() {}
             }
         });
@@ -45,15 +50,20 @@ pub fn bench(
         s.spawn(|| {
             core_affinity::set_for_current(ping_core);
 
-            // Wait for Pong and start the timer
-            while state.load(Ordering::Relaxed) == WaitForPong as u8 {}
-            let start = Instant::now();
+            let mut results = Vec::with_capacity(num_samples as usize);
 
-            for _ in 0..num_round_trips+1 {
-                while state.compare_exchange(Pong as u8, Ping as u8, Ordering::Relaxed, Ordering::Relaxed).is_err() {}
+            // Wait for Pong
+            while state.load(Ordering::Relaxed) == WaitForPong as u8 {}
+
+            for _ in 0..num_samples {
+                let start = Instant::now();
+                for _ in 0..num_round_trips {
+                    while state.compare_exchange(Pong as u8, Ping as u8, Ordering::Relaxed, Ordering::Relaxed).is_err() {}
+                }
+                results.push(start.elapsed());
             }
 
-            start.elapsed()
+            results
         })
         .join()
         .unwrap()
@@ -62,37 +72,48 @@ pub fn bench(
 
 #[derive(clap::Parser)]
 struct Args {
-    /// The number of iterations
-    #[clap(default_value_t = DEFAULT_NUM_ROUND_TRIP, value_parser)]
-    num_round_trips: RoundTrips,
+    /// The number of iterations per sample
+    #[clap(default_value_t = DEFAULT_NUM_ROUND_TRIPS, value_parser)]
+    num_round_trips: Count,
 
-    /// Outputs CSV results on stdout
+    /// The number of samples
+    #[clap(default_value_t = DEFAULT_NUM_SAMPLES, value_parser)]
+    num_samples: Count,
+
+    /// Outputs the mean latencies in CSV format on stdout
     #[clap(long, value_parser)]
-    csv: bool
+    csv: bool,
 }
 
 fn main() {
-    let args = Args::parse();
+    let Args { num_samples, num_round_trips, csv } = Args::parse();
 
     let cores = core_affinity::get_core_ids().expect("get_core_ids() failed");
     assert!(cores.len() >= 2);
+    let n_cores = cores.len();
 
     #[cfg(feature = "rdtsc")]
     let tsc = minstant::is_tsc_available();
     #[cfg(not(feature = "rdtsc"))]
     let tsc = false;
 
-    eprintln!("Num cores: {}", cores.len());
-    eprintln!("Num round trips: {}", args.num_round_trips);
+    eprintln!("Num cores: {}", n_cores);
+    eprintln!("Num samples: {}", num_samples);
+    eprintln!("Num round trips per samples: {}", num_round_trips);
     eprintln!("Using RDTSC to measure time: {}", tsc);
-    eprintln!("Showing round-trip-time/2 in nanoseconds:");
 
-    let mut results = Vec::new();
+    let shape = ndarray::Ix3(n_cores, n_cores, num_samples as usize);
+    let mut results = ndarray::Array::from_elem(shape, f64::NAN);
+
+    // Warmup
+    bench((cores[0], cores[1]), 300, 3);
 
     // First print the column header
-    eprint!("{: >5}", "");
+    eprintln!("Showing latency=round-trip-time/2 in nanoseconds:");
+    eprintln!();
+    eprint!("{: >3}", "");
     for j in &cores {
-        eprint!("{: >5}", j.id);
+        eprint!(" {: >4}{: >3}", j.id, "");
         //        |||
         //        ||+-- Width
         //        |+--- Align
@@ -100,32 +121,76 @@ fn main() {
     }
     eprintln!();
 
-    // Warmup
-    bench((cores[0], cores[1]), 1000);
+    let mcolor = Color::White.bold();
+    let scolor = Color::White.dimmed();
 
-    for i in &cores {
-        let mut row = Vec::new();
-        eprint!("{: >5}", i.id);
-        for j in &cores {
-            let duration = if i.id != j.id {
-                let duration = bench((*i, *j), args.num_round_trips);
-                let duration = (duration.as_nanos() as f64) / (args.num_round_trips as f64) / 2.0;
-                eprint!("{: >5.0}", duration);
-                duration
-            } else {
-                eprint!("{: >5}", "");
-                f64::NAN
-            };
-            let _ = std::io::stdout().lock().flush();
-            row.push(duration);
+    // Do the benchmark
+    for i in 0..n_cores {
+        let core_i = cores[i];
+        eprint!("{: >3}", core_i.id);
+        for j in 0..n_cores {
+            if i > j {
+                let core_j = cores[j];
+                // We add 1 warmup cycle first
+                let durations = bench((core_i, core_j), num_round_trips, 1+num_samples);
+                let durations = &durations[1..];
+                let mut values = results.slice_mut(s![i,j,..]);
+                for s in 0..num_samples as usize {
+                    values[s] = durations[s].as_nanos() as f64 / (num_round_trips as f64) / 2.0;
+                }
+
+                let mean = format!("{: >4.0}", values.mean().unwrap());
+                // We apply the central limit therem to estimate the standard deviation
+                let stddev = format!("±{: <2.0}", values.std(1.0).min(99.0) / (num_samples as f64).sqrt());
+                eprint!(" {}{}", mcolor.paint(mean), scolor.paint(stddev));
+                let _ = std::io::stdout().lock().flush();
+            }
         }
-        results.push(row);
         eprintln!();
     }
+    eprintln!();
 
-    if args.csv {
-        for row in &results {
-            println!("{}", row.iter().map(|v| v.to_string()).collect::<Vec<_>>().join(","));
+    // Print min/max stats
+    {
+        let mean = results.mean_axis(Axis(2)).unwrap();
+        let stddev = results.std_axis(Axis(2), 1.0) / (num_samples as f64).sqrt();
+
+        let ((min_i, min_j), _) = mean.indexed_iter()
+            .filter_map(|(i, v)| NotNan::new(*v).ok().map(|v| (i, v)))
+            .min_by_key(|(_, v)| *v)
+            .unwrap();
+        let min_mean = format!("{:.1}", mean[(min_i, min_j)]);
+        let min_stddev = format!("±{:.1}", stddev[(min_i, min_j)]);
+        let (min_core_id_i, min_core_id_j) = (cores[min_i].id, cores[min_j].id);
+
+        let ((max_i, max_j), _) = mean.indexed_iter()
+            .filter_map(|(i, v)| NotNan::new(*v).ok().map(|v| (i, v)))
+            .max_by_key(|(_, v)| *v)
+            .unwrap();
+        let max_mean = format!("{:.1}", mean[(max_i, max_j)]);
+        let max_stddev = format!("±{:.1}", stddev[(max_i, max_j)]);
+        let (max_core_id_i, max_core_id_j) = (cores[max_i].id, cores[max_j].id);
+
+        eprintln!("Min  latency: {}ns {} cores: ({},{})", mcolor.paint(min_mean), scolor.paint(min_stddev), min_core_id_i, min_core_id_j);
+        eprintln!("Max  latency: {}ns {} cores: ({},{})", mcolor.paint(max_mean), scolor.paint(max_stddev), max_core_id_i, max_core_id_j);
+    }
+
+    // Print mean stats
+    {
+        let values = results.iter().copied().filter(|v| !v.is_nan()).collect::<Vec<_>>();
+        let values = ndarray::arr1(&values);
+        let mean = format!("{:.1}", values.mean().unwrap());
+        // no stddev, it's hard to put a value that is is meaningful without explanation
+        eprintln!("Mean latency: {}ns", mcolor.paint(mean));
+    }
+
+    if csv {
+        let results = results.mean_axis(Axis(2)).unwrap();
+        for row in results.rows() {
+            let row = row.iter()
+                .map(|v| v.is_nan().then_some("".to_string()).unwrap_or(v.to_string()))
+                .collect::<Vec<_>>().join(",");
+            println!("{}", row);
         }
     }
 }
